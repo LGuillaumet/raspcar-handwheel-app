de455a832f637c0d3ab1ad8492fe35c1cb4996f5
Steven <steven.bouche@outlook.com>
2022-03-27 20:01:55 +0200

    spa simulator

---
+++ simulator-spa/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { CoreModule } from './core/core.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ simulator-spa/src/app/core/simulator/simulator.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { SimulatorService } from './simulator.service';
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  providers: [
+    SimulatorService
+  ]
+})
+export class SimulatorModule { }
