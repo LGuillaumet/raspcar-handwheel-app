2a19b53d403fcce05bbf0b4e567fb4d3165f1cda
raspberryPi <leo.guillaumet@etu.unice.fr>
2022-03-29 13:33:26 +0200

    update react on rasp

--- scripts/start_webinterface_spa.sh
+++ scripts/start_webinterface_spa.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-cd ../react-build
+cd /home/pi/Documents/raspcar-handwheel-app/react-build
 http-server --port 3000 -a 0.0.0.0
+chromium-browser --start-fullscreen http://localhost:3000
--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+
 ./vcan_script.sh
 ./start_simulator.sh &
 ./start_simulator_spa.sh &
-# ./start_main_server.sh &
+./start_main_server.sh &
+./start_webinterface_spa.sh &
 candump vcan_tx &
 candump vcan_rx
 
